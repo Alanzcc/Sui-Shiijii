af92aed373557036b7784f661d3ddf47e4ad0322
alanzcc <alanzcc1@gmail.com>
2024-09-16 15:21:34 -0300

    Starting lighting

--- lib/Shader.h
+++ lib/Shader.h
@@ -2,7 +2,8 @@
 #define SHADER_H
 
 #include <glad.h>
-
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <fstream>
 #include <sstream>
--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "shader.h"
-#include "camera.h"
-
+#include "Shader.h"
+#include "Camera.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+#include "Vertex.h"
 
 #include <iostream>
 
@@ -131,9 +134,11 @@ int main()
         glm::vec3( 1.5f,  0.2f, -1.5f),
         glm::vec3(-1.3f,  1.0f, -1.5f)
     };
-    unsigned int VBO, VAO;
+    unsigned int VAO, VBO;
+
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+
 
     glBindVertexArray(VAO);
 
@@ -141,12 +146,14 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),  (void*)0);
     glEnableVertexAttribArray(0);
+
     // texture coord attribute
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // load and create a texture
     // -------------------------
@@ -264,8 +271,10 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
+
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
